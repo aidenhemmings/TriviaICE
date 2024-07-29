087322ad83577d8ec25988c91a61ee1a0f1dd86d
Tristan Stavast <stavasttristan@gmail.com>
2024-07-29 09:57:16 +0200

    Added pages

--- app/src/main/java/com/example/triviaice/MainActivity.kt
+++ app/src/main/java/com/example/triviaice/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.triviaice
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,9 +17,25 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        var btnGenKnowledge = findViewById<Button>(R.id.btnGenKnowledge)
+        var btnFilms = findViewById<Button>(R.id.btnFilms)
+        var btnGames = findViewById<Button>(R.id.btnGames)
+
+        btnGenKnowledge.setOnClickListener {
+            val intent = Intent(this, GenKnowledge::class.java)
+            startActivity(intent)
+        }
+        btnFilms.setOnClickListener {
+            val intent = Intent(this, Films::class.java)
+            startActivity(intent)
+        }
+        btnGames.setOnClickListener {
+            val intent = Intent(this, Games::class.java)
+            startActivity(intent)
+        }
+
     }
 
-        
 
-}
 }
