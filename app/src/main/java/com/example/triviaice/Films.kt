d4178080ecbc997848ed8eeb9ce3735d2728f5c6
Tristan Stavast <stavasttristan@gmail.com>
2024-08-01 10:13:27 +0200

    Scroll View and Linear View added for films

--- app/src/main/java/com/example/triviaice/Films.kt
+++ app/src/main/java/com/example/triviaice/Films.kt
@@ -1,12 +1,27 @@
 package com.example.triviaice
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Films : AppCompatActivity() {
+
+    private lateinit var questionText : TextView
+    private lateinit var answersGroup : RadioGroup
+    private lateinit var filmAnswer1 : RadioButton
+    private lateinit var filmAnswer2 : RadioButton
+    private lateinit var filmAnswer3 : RadioButton
+    private lateinit var filmAnswer4 : RadioButton
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +31,8 @@ class Films : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+
     }
 }
